4177e5f941352606492187832a920b6f468eba87
Serg75 <activequest@gmail.com>
2026-03-09 13:25:10 +0300

    rur

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV4 {
     @Test
@@ -20,6 +22,10 @@ class MobileBankApiTestV4 {
                 // Включаем логирование ответа
                 .log().all()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[0].id", equalTo(1))
+                .body("", hasSize(3))
+                .body("[0].name", equalTo("Текущий счёт"))
+                .body("[0].currency", equalTo("RUB"));
     }
 }
